8dfd44b23efb63a51bc48942ca8af3715cf6fe4c
kasdfg2003 <kasdfg2003@naver.com>
2022-06-28 13:57:12 +0900

    testExample

--- chap03/src/sec03/exam02_increase_decrease/IncreaseDecreaseOperatorExample.java
+++ chap03/src/sec03/exam02_increase_decrease/IncreaseDecreaseOperatorExample.java
@@ -18,7 +18,7 @@ public class IncreaseDecreaseOperatorExample {
 		System.out.println("y=" + y);
 		
 		System.out.println("--------------------------");
-		z = x++;
+		z = x++;           // 대입먼저 후에 더하기 1
 		System.out.println("z=" + z);
 		System.out.println("x=" + x);
 		
@@ -28,10 +28,10 @@ public class IncreaseDecreaseOperatorExample {
 		System.out.println("x=" + x);
 		
 		System.out.println("--------------------------");
-		z = ++x + y++;
+		z = ++x + y++;   // 15 + 8 = 23 이지만 후에 y더하기1
 		System.out.println("z=" + z);
 		System.out.println("x=" + x);
-		System.out.println("y=" + y);
+		System.out.println("y=" + y); // 출력값 9 
 		
 	}
 
